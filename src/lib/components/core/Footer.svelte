e4bc308dde2c55e56ef452bc691ad789f6db0628
Deahtstroke <dvm3099@pm.me>
2026-06-02 20:46:36 -0700

    Changed github url

--- src/lib/components/core/Footer.svelte
+++ src/lib/components/core/Footer.svelte
@@ -1,42 +1,43 @@
 <script lang="ts">
-  const GITHUB_URL: string = "https://github.com/dvillavicencio";
+	const GITHUB_URL: string = 'https://github.com/deahtstroke';
 </script>
 
-<style>
-footer {
-  display: grid;
-  grid-template-columns: 1fr auto 1fr;
-  margin-top: auto;
-  background-color: var(--background-color);
-}
-
-.madeBy {
-  grid-column: 2;
-}
-
-a {
-  color: var(--text-color);
-  text-decoration: none;
-}
-
-a:hover {
-  text-decoration: underline;
-}
-
-a:visited {
-  color: var(--text-color);
-  text-decoration: none;
-}
-
-p {
-  color: var(--text-color);
-}
-</style>
-
 <footer>
-  <div class="madeBy">
-    <p>Made by 
-      <a href={GITHUB_URL} target="blank">Deahtstroke</a>
-    </p>
-  </div> 
+	<div class="madeBy">
+		<p>
+			Made by
+			<a href={GITHUB_URL} target="blank">Deahtstroke</a>
+		</p>
+	</div>
 </footer>
+
+<style>
+	footer {
+		display: grid;
+		grid-template-columns: 1fr auto 1fr;
+		margin-top: auto;
+		background-color: var(--background-color);
+	}
+
+	.madeBy {
+		grid-column: 2;
+	}
+
+	a {
+		color: var(--text-color);
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+
+	a:visited {
+		color: var(--text-color);
+		text-decoration: none;
+	}
+
+	p {
+		color: var(--text-color);
+	}
+</style>
